59a3d4538b3b968704758393062305a3bff44fa0
seraf7 <zaske_4@hotmail.com>
2019-10-01 10:57:21 -0500

    ejercicio Segues

---
+++ seguesProgram/seguesProgram/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  seguesProgram
+//
+//  Created by 2020-1 on 9/18/19.
+//  Copyright © 2019 Conde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue){
+        
+    }
+    
+}
+
